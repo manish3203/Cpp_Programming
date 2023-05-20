658b9107ba1f9de56260fb52a2ed96b26e9a6ee6
manish3203 <manishchavan3203@gmail.com>
2023-05-20 21:27:43 +0530

    Variables Codes In CPP

---
+++ Variable/Class_Codes/1_Program.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main() {
+
+	int x = 10;
+	int y(20);
+
+	std :: cout << x << std :: endl;
+	std :: cout << y << std :: endl;
+
+	int z{30};
+	
+	std :: cout << z << std :: endl;
+
+	return 0;
+}
+
---
+++ Variable/Class_Codes/2_Program.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+int main() {
+
+	int x = 10;
+	int y = 20.5f;
+
+	std :: cout << x << std :: endl;
+	std :: cout << y << std :: endl;
+
+	int a{10};
+	int b{10.5f};
+	
+	std :: cout << a << std :: endl;
+	std :: cout << b << std :: endl;
+
+	return 0;
+}
+
+/* error: narrowing conversion of ‘1.05e+1f’ from ‘float’ to ‘int’ [-Wnarrowing]
+   13 |  int b{10.5f};
+*/
