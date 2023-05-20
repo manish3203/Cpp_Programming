1bcab837303da6b544290d2ae91982373771faa5
manish3203 <manishchavan3203@gmail.com>
2023-05-20 22:00:59 +0530

    Reference Variable Initialize In CPP

---
+++ Variable/Class_Codes/5_Program.cpp
@@ -0,0 +1,14 @@
+//Reference Variable 
+
+#include <iostream>
+
+int main() {
+
+	int x = 10;
+	int &y = x;
+
+	std :: cout << x << std :: endl;
+	std :: cout << y << std :: endl;
+
+	return 0;
+}
---
+++ Variable/Class_Codes/6_Program.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main() {
+
+	int x = 10;
+	int &y;
+	int *ptr = NULL;
+}
+
+/* error: ‘y’ declared as reference but not initialized
+    7 |  int &y;
+      |       ^
+
+*/
